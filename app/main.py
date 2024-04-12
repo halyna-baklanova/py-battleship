d01dc6a524967842e7a68cc36bc58ef4a384875d
Halyna Baklanova <novabalka@gmail.com>
2024-04-12 13:25:44 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,9 +4,6 @@ class Deck:
         self.column = column
         self.is_alive = is_alive
 
-    def __str__(self) -> str:
-        return f"({self.row}, {self.column}) - {'Alive' if self.is_alive else 'Destroyed'}"
-
 
 class Ship:
     def __init__(
@@ -34,20 +31,17 @@ class Ship:
 
     def fire(self, row: int, column: int) -> None:
         self.get_deck(row, column).is_alive = False
-        print([deck.is_alive for deck in self.decks])
-        if all(deck.is_alive for deck in self.decks):
+        if all(not deck.is_alive for deck in self.decks):
             self.is_drowned = True
-        print(self.is_drowned)
-        print()
 
 
 class Battleship:
-    def __init__(self, ships: tuple) -> None:
-        self.field = {
-            (deck.row, deck.column):
-                Ship(start=ship[0], end=ship[1])
-            for ship in ships
-            for deck in Ship(start=ship[0], end=ship[1]).decks}
+    def __init__(self, ships: list) -> None:
+        self.field = {}
+        for ship_start, ship_end in ships:
+            ship = Ship(start=ship_start, end=ship_end)
+            for deck in ship.decks:
+                self.field[(deck.row, deck.column)] = ship
         self.ships = ships
 
     def fire(self, location: tuple) -> str:
@@ -58,32 +52,3 @@ class Battleship:
                 return "Sunk!"
             return "Hit!"
         return "Miss!"
-
-
-ships = [
-    ((2, 0), (2, 3)),
-    ((4, 5), (4, 6)),
-    ((3, 8), (3, 9)),
-    ((6, 0), (8, 0)),
-    ((6, 4), (6, 6)),
-    ((6, 8), (6, 9)),
-    ((9, 9), (9, 9)),
-    ((9, 5), (9, 5)),
-    ((9, 3), (9, 3)),
-    ((9, 7), (9, 7)),
-]
-
-# Створення об'єкту Battleship з заданими кораблями
-battlefield = Battleship(ships)
-
-coordinates = [
-    (2, 0),
-    (2, 1),
-    (2, 2),
-    (2, 3),
-
-]
-
-for coord in coordinates:
-    result = battlefield.fire(coord)
-    print(result)
